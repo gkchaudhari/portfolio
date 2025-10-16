569ef8cf2e2e9aa905d314bea25609e1cc461a5f
gchaudhari2111 <gchaudhari2111@gmail.com>
2025-10-16 17:36:11 +0000

    optimize code

--- portfoilo/src/app/dashboard/dashboard.component.ts
+++ portfoilo/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from '../skills/skills.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { ResumeComponent } from '../resume/resume.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SkillsComponent],
+  imports: [SkillsComponent, MatStepperModule, ResumeComponent, NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+    isLinear: boolean = false;
 }
--- portfoilo/src/app/navbar/navbar.component.ts
+++ portfoilo/src/app/navbar/navbar.component.ts
@@ -1,35 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import {MatStepperModule} from '@angular/material/stepper';
-import { ResumeComponent } from '../resume/resume.component';
 interface INavlink{
     title:string,
     link:string
 }
 @Component({
   selector: 'app-navbar',
-  imports: [MatStepperModule,ResumeComponent],
+  imports: [MatStepperModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-    isLinear = false;
-    nvaLinks: INavlink[] = [
-        {
-          title:'Dashboard',
-          link:'dashboard'
-        },
-        {
-          title:'Projects',
-          link:'projects'
-        },
-        {
-          title:'Skills',
-          link:'skills'
-        },
-        {
-          title:'Resume',
-          link:'resume'
-        },
-    ]
 }
