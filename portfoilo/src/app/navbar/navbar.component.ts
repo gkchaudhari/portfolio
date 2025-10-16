dc9606e226c560b79ad1e8b2c5581ac8683b4972
gchaudhari2111 <gchaudhari2111@gmail.com>
2025-10-16 07:27:23 +0000

    tech icons support

--- portfoilo/src/app/navbar/navbar.component.ts
+++ portfoilo/src/app/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatStepperModule} from '@angular/material/stepper';
+import { ResumeComponent } from '../resume/resume.component';
 interface INavlink{
     title:string,
     link:string
 }
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [MatStepperModule,ResumeComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    isLinear = false;
     nvaLinks: INavlink[] = [
         {
           title:'Dashboard',
