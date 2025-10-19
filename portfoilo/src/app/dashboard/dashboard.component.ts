7bff40c8de93530e2964849a836d633dfe2e0dd3
ganeshbyte <gc930954@gmail.com>
2025-10-19 11:39:03 +0530

    support for the footer and reordering the stepper sections

--- portfoilo/src/app/dashboard/dashboard.component.ts
+++ portfoilo/src/app/dashboard/dashboard.component.ts
@@ -3,10 +3,13 @@ import { SkillsComponent } from '../skills/skills.component';
 import { MatStepperModule } from '@angular/material/stepper';
 import { ResumeComponent } from '../resume/resume.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ExperienceComponent } from "./experience/experience.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SkillsComponent, MatStepperModule, ResumeComponent, NavbarComponent],
+  imports: [ MatStepperModule, ResumeComponent, NavbarComponent, ExperienceComponent, ContactComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- portfoilo/src/app/resume/resume.component.ts
+++ portfoilo/src/app/resume/resume.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-resume',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.scss'
+  styleUrl: './resume.component.scss',
 })
 export class ResumeComponent {
-    
-  downloadTemplate(){
-    const filePath: string = "assets/docs/resume.pdf";
-    const fileName: string = "resume.pdf";
-    const link = document.createElement('a');
-    link.href = filePath;
-    link.download = fileName;
-    link.click();
-    console.log("dowloading template...");
-    document.body.removeChild(link);
+  isDownloadLoading = signal<boolean>(false);
+
+  downloadTemplate() {
+    this.isDownloadLoading.set(true);
+    setTimeout(() => {
+      const filePath: string = 'assets/docs/resume.pdf';
+      const fileName: string = 'resume.pdf';
+      const link = document.createElement('a');
+      link.href = filePath;
+      link.download = fileName;
+      link.click();
+      console.log('Downloading template...');
+
+      this.isDownloadLoading.set(false);
+      document.body.removeChild(link);
+    }, 2000);
   }
 }
